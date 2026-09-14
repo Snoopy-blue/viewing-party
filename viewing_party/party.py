0641b74d5f6405d8c8b987839a4812dfcd7786b5
Yan <yanzhaocd@gmail.com>
2026-09-14 15:34:48 -0700

    finished 6/9 tests in Wave 1. We were stuck in test7 in Wave 1. Push the updates to the git.

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,40 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+    movie = {}
+
+    if title and genre and rating:
+        movie["title"] = title
+        movie["genre"] = genre
+        movie["rating"] = rating
+    else:
+        return None
+
+    return movie
+
+def add_to_watched(user_data, movie):
+    user_data["watched"].append(movie) 
+
+    return user_data
+
+def add_to_watchlist(user_data, movie):
+    user_data["watchlist"].append(movie) 
+    
+    return user_data
+
+def watch_movie(user_data, title):
+    for movie in user_data["watchlist"]: # Loop through movies in a list. 
+        # user_data["watchlist"] = list contains mutiple dictionaries
+        # movie = each element of the list, dictionary contains title, genere, rating. 
+
+        if movie["title"] == title: # dictionary[key], value of "title"
+            #movie_to_watch = movie
+            user_data["watchlist"].remove(movie)
+            user_data["watched"].append(movie)
+            return user_data
+
+    return user_data
+            
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
@@ -20,4 +53,3 @@ def create_movie(title, genre, rating):
 # -----------------------------------------
 # ------------- WAVE 5 --------------------
 # -----------------------------------------
-
