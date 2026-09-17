10a5b3b058859ea48bc0083cf1dbc1647cb48619
Daihuizi Zhu <zhudaihuizi@gmail.com>
2026-09-17 11:50:42 -0700

    Complete Wave 05

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -119,3 +119,24 @@ def get_available_recs(user_data):
 # -----------------------------------------
 # ------------- WAVE 5 --------------------
 # -----------------------------------------
+def get_new_rec_by_genre(user_data):
+    recommendations = []
+    most_watched_genre = get_most_watched_genre(user_data)
+    friends_unique_movies = get_friends_unique_watched(user_data)
+    for movie in friends_unique_movies:
+        if movie["genre"] == most_watched_genre:
+            recommendations.append(movie)
+
+
+    return recommendations
+
+def get_rec_from_favorites(user_data):
+    recommendations = []
+
+    friends_watched_list = get_friends_watched_list(user_data)
+
+    for movie in user_data["favorites"]:
+        if movie not in friends_watched_list:
+            recommendations.append(movie)
+
+    return recommendations
